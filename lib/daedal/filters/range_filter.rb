d0191da2cf9929b4d36a5fa88906c60ece1902dd
Marcos Alcantara <marcos.alcantara@flippa.com>
2020-06-01 12:19:52 +1000

    Add the format and timezone range attributes

--- lib/daedal/filters/range_filter.rb
+++ lib/daedal/filters/range_filter.rb
@@ -2,7 +2,7 @@ module Daedal
   module Filters
     """Class for the basic term filter"""
     class RangeFilter < Filter
-  
+
       # required attributes
       attribute :field, Daedal::Attributes::Field
 
@@ -11,6 +11,8 @@ module Daedal
       attribute :lte,   Daedal::Attributes::QueryValue, required: false
       attribute :gt,    Daedal::Attributes::QueryValue, required: false
       attribute :lt,    Daedal::Attributes::QueryValue, required: false
+      attribute :timezone, Daedal::Attributes::QueryValue, required: false
+      attribute :format, Daedal::Attributes::QueryValue, required: false
 
       def initialize(options={})
         super options
@@ -18,9 +20,9 @@ module Daedal
           raise "Must give at least one of gte, gt, lt, or lte"
         end
       end
-  
+
       def to_hash
-        {range: {field => {gte: gte, lte: lte, lt: lt, gt: gt}.select {|k,v| !v.nil?}}}
+        {range: {field => {gte: gte, lte: lte, lt: lt, gt: gt, format: format, timezone: timezone}.select {|k,v| !v.nil?}}}
       end
     end
   end
